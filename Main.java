b6ae6997df843e9eca7a1ab3f9953d9fa778180a
Pedro Rian <49865204+Pleonasmo@users.noreply.github.com>
2023-10-30 10:48:15 -0300

    Função de inserir alterada (entrada de dados provisoria)

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 import arvore.Arvore;
+import arvore.No;
 
 public class Main {
 
@@ -20,27 +22,48 @@ public class Main {
         return linha;
     }
 
+    public static void imprimirEmOrdem(No no) {
+        if (no != null) {
+            imprimirEmOrdem(no.getFilhoEsquerda());
+            System.out.print(no.getValor() + " ");
+            imprimirEmOrdem(no.getFilhoDireita());
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        String arqLeituraArvores = "Arquivos/arquivo1.txt";
-        String arqLeituraFuncoes = "Arquivos/arquivo2.txt";
+        // String arqLeituraArvores = "Arquivos/arquivo1.txt";
+        // String arqLeituraFuncoes = "Arquivos/arquivo2.txt";
 
-        String linhasArq = lerArquivo(arqLeituraArvores);
-        System.out.println(linhasArq);
-        Arvore essa;
+        // String linhasArq = lerArquivo(arqLeituraArvores);
+        // System.out.println(linhasArq);
+        // Arvore essa;
 
-        ArrayList<Arvore> arvores = new ArrayList<>();
+        // ArrayList<Arvore> arvores = new ArrayList<>();
 
-        int contador = 0;
-        String[] nos = linhasArq.split(" ");
-        System.out.println(Integer.parseInt(nos[contador]));
-        essa = new Arvore(Integer.parseInt(nos[contador]));
-        for (String no : nos) {
-System.out.println("Entrou aqui - for");
-            essa = essa.inserirElemento(Integer.parseInt(nos[contador++]), essa.getRaiz(), essa);
+        // int contador = 0;
+        // String[] nos = linhasArq.split(" ");
+        // System.out.println(Integer.parseInt(nos[contador]));
+        // essa = new Arvore(Integer.parseInt(nos[contador]));
+        // for (String no : nos) {
+        // System.out.println("Entrou aqui - for");
+        // essa = essa.inserirElemento(Integer.parseInt(nos[contador++]),
+        // essa.getRaiz(), essa);
+        // }
+
+        // String arv = essa.imprimeArvore("", essa.getRaiz());
+        // JOptionPane.showMessageDialog(null, arv);
+
+        Arvore a1 = new Arvore();
+        Scanner ler = new Scanner(System.in);
+
+        int i = 1;
+        while (i > 0) {
+            System.out.println("Digite o valor a ser inserido na árvore:");
+            i = ler.nextInt();
+            a1.inserirElemento(i);
         }
 
-        String arv = essa.imprimeArvore("", essa.getRaiz());
-        JOptionPane.showMessageDialog(null, arv);
+        imprimirEmOrdem(a1.getRaiz());
 
     }
 
--- arvore/Arvore.java
+++ arvore/Arvore.java
@@ -3,52 +3,35 @@ package arvore;
 public class Arvore {
     private No raiz;
 
-    public Arvore(int valor) {
-        this.raiz = new No(valor);
-        raiz.setNivel(1);
+    public Arvore() {
+        this.raiz = null;
     }
 
-    public int buscarElemento(No no, int elemento) {
-        if (no.getValor() == elemento) {
+    public int buscarElemento(No no, int valor) {
+        if (no.getValor() == valor)
             return 1;
-        } else if (no.getFilhoDireita() != null) {
+        else if (no.getFilhoDireita() != null)
+            buscarElemento(no.getFilhoDireita(), valor);
+        else if (no.getFilhoEsquerda() != null)
+            buscarElemento(no.getFilhoEsquerda(), valor);
 
-            buscarElemento(no.getFilhoDireita(), elemento);
-
-        } else if (no.getFilhoEsquerda() != null) {
-
-            buscarElemento(no.getFilhoEsquerda(), elemento);
-
-        }
         return 0;
     }
 
-    public Arvore inserirElemento(int elemento, No no, Arvore arvore) {
-        System.out.println(no.getValor());
-        System.out.println("Elemnteo: " + elemento);
-        if (elemento < no.getValor()) {
-            System.out.println("Entrou no if esq");
-            if (no.getFilhoEsquerda() != null) {
-                System.out.println("Entrou no segund if esq");
-               return inserirElemento(elemento, no.getFilhoEsquerda(), arvore);
-            } else {
-                 System.out.println("Entrou no else");
-                no.setFilhoEsquerda(new No(elemento));
-            }
-        }
-        if (elemento > no.getValor()) {
-            System.out.println("Entrou no if dir");
-            if (no.getFilhoDireita() != null) {
-                System.out.println("Entrou no segund if dir");
-                return inserirElemento(elemento, no.getFilhoDireita(), arvore);
-            } else {
-                System.out.println("Entrou no else");
-                no.setFilhoDireita(new No(elemento));
-            }
-        }
-        System.out.println("Passou dos dois ifs.");
-        return arvore;
+    public void inserirElemento(int valor) {
+        this.raiz = inserirElemento(valor, this.raiz);
+    }
 
+    public No inserirElemento(int valor, No no) {
+        if (no == null) {
+            no = new No(valor);
+            return no;
+        } else if (valor < no.getValor())
+            no.setFilhoEsquerda(inserirElemento(valor, no.getFilhoEsquerda()));
+        else if (valor > no.getValor())
+            no.setFilhoDireita(inserirElemento(valor, no.getFilhoDireita()));
+
+        return no;
     }
 
     void removerElemento(int elemento) {
@@ -99,7 +82,7 @@ public class Arvore {
             }
         }
         String arv = "";
-   
+
         for (int i = 0; i < linha.length; i++) {
             arv += linha[i];
         }
@@ -110,7 +93,7 @@ public class Arvore {
 
     public String imprimeArvore(String a, No no) {
         String arvore = a;
-   
+
         arvore = organizarTraco(no);
         if (no.getFilhoDireita() != null) {
             arvore += imprimeArvore(arvore, no.getFilhoDireita());
